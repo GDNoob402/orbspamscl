2e513bb824a0756459ff883de3f374166022f707
shafikdabest1 <shafikkhalil1907@gmail.com>
2026-09-13 13:37:13 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -140,6 +140,9 @@ export default {
                     <p>
                        no random triggers
                     </p>
+                     <p>
+                        levels must be possible in under 16 cps, do NOT use a loophole to get around this, use common sense to determine the rule's use
+                    </p>
                     <p>
                         try not to use any art importer mods because they can break level id's
                     </p>
